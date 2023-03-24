d4ee1caa6b9b8f4ce948b45b54130785bcc65637
muhammadsadry19@gmail.com <muhammadsadry19@gmail.com>
2023-03-24 14:39:51 +0700

    Assessment 1

---
+++ app/src/main/java/org/d3if3031/mystartup/SignUpActivity.kt
@@ -0,0 +1,39 @@
+package org.d3if3031.mystartup
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.util.Log
+import android.widget.Toast
+import org.d3if3031.mystartup.databinding.ActivitySignUpBinding
+
+class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var Username: String
+    private lateinit var Email: String
+    private lateinit var Phone: String
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_sign_up)
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnRegister.setOnClickListener{
+            Username = binding.tfUserName.text.toString()
+            Email = binding.tfemail.text.toString()
+            Phone = binding.tfphone.text.toString()
+
+            val intent = Intent(this, DataProfile::class.java)
+            intent.putExtra("Username", Username)
+            intent.putExtra("Email", Email)
+            intent.putExtra("Phone", Phone)
+            startActivity(intent)
+
+        }
+    }
+}
